6da2b3c38ccfc983b9a1169bf5efc7af7f415c29
DouGe <douge@example.com>
2026-04-05 11:18:32 +0800

    chore: 移除main.js中的Element Plus引入和注册，前端完全迁移至Arco Design

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,19 +1,10 @@
-﻿import { createApp } from 'vue'
-import ArcoVue from '@arco-design/web-vue'
-import '@arco-design/web-vue/dist/arco.css'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import App from './App.vue'
-import router from './router'
+﻿import { createApp } from '\''vue'\''
+import ArcoVue from '\''@arco-design/web-vue'\''
+import '\''@arco-design/web-vue/dist/arco.css'\''
+import App from '\''./App.vue'\''
+import router from '\''./router'\''
 
 const app = createApp(App)
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
 app.use(ArcoVue)
-app.use(ElementPlus)
 app.use(router)
-app.mount('#app')
+app.mount('\''#app'\'')
